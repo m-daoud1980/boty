f4634483a643e79dc2df51a084cdbbb586399e90
m-daoud1980 <m.daoud1980@yahoo.com>
2025-04-15 09:51:58 +0200

    Update bot.py
    fix port

--- bot.py
+++ bot.py
@@ -227,4 +227,4 @@ def log_stream(symbol):
     return Response(event_stream(), mimetype="text/event-stream")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=10000)
